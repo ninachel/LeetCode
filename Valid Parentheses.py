3dfa2e16b25d43c0f1fdb8a1c38e7f3e39166906
ninachel <nccccc17@gmail.com>
2020-12-21 02:32:58 +0300

    -

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,13 +1,23 @@
-class Solution(object):
-    def isValid(self, s):
-        pairs = {'(': ')', '{': '}', '[': ']'}
-        stack = [x for x in s]
-        # stack_copy = stack[:]
-        # for i in range(1, len(stack), 2):
-        #     if stack[i] == pairs[stack[i - 1]]:
-        #         stack_copy = stack_copy[:i - 1] + stack_copy[i + 1:]
-        # return stack_copy
+def is_valid(s):
+    left = {'(': ')', '{': '}', '[': ']'}
+    right = {')': '(', '}': '{', ']': '['}
+    stack = []
+    i = 0
+    if len(s) % 2 != 0:
+        return False
+    while i < len(s):
+        stack.append(s[i])
+        if s[i] in left and i <= len(s) - 2:
+            if s[i + 1] == left[s[i]]:
+                stack.pop()
+                i += 1
+        elif s[i] in right and len(stack) >= 2:
+            if stack[-2] == right[s[i]]:
+                stack.pop()
+                stack.pop()
+        i += 1
+    return not stack
 
 
-res = Solution()
-print(res.isValid("{[]}"))
+s = "){"
+print(is_valid(s))
